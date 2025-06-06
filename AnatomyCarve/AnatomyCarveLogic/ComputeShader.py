36b4340fc54fa7bee1ed56385321b20cedf273f2
andrey-titov <andrey-titov@users.noreply.github.com>
2025-06-06 17:05:57 -0400

    Adding Texture3D class to manage textures

--- AnatomyCarve/AnatomyCarveLogic/ComputeShader.py
+++ AnatomyCarve/AnatomyCarveLogic/ComputeShader.py
@@ -44,16 +44,5 @@ class ComputeShader:
         glLinkProgram(prog)
         return prog
     
-    def uploadVolumeToTexture(volumeNode : vtkMRMLScalarVolumeNode):
-        array = slicer.util.arrayFromVolume(volumeNode).astype(np.float32)
-        dims = array.shape[::-1]
-        texID = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_3D, texID)
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-        glTexStorage3D(GL_TEXTURE_3D, 1, GL_R32F, *dims)
-        glTexSubImage3D(GL_TEXTURE_3D, 0, 0, 0, 0, *dims, GL_RED, GL_FLOAT, array.ravel())
-        return texID, dims, array.shape
-    
     def print(self):
         pass
--- AnatomyCarve/AnatomyCarveLogic/__init__.py
+++ AnatomyCarve/AnatomyCarveLogic/__init__.py
@@ -1,3 +1,4 @@
 from AnatomyCarveLogic.AnatomyCarveLogic import AnatomyCarveLogic
 from AnatomyCarveLogic.AnatomyCarveLogic import AnatomyCarveParameterNode
 from AnatomyCarveLogic.ComputeShader import ComputeShader
+from AnatomyCarveLogic.Texture import Texture3D
